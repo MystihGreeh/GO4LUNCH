5ddc48b17052f753d2c30319f0321926a0d19534
MystihGreeh <charlotte.achin@gmail.com>
2020-12-14 21:50:04 +0100

    Google and Facebook auth are working

--- app/src/main/java/com/mystihgreeh/go4lunch/LoginActivity.java
+++ app/src/main/java/com/mystihgreeh/go4lunch/LoginActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 
 import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.common.SignInButton;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
@@ -14,11 +15,15 @@ import com.facebook.FacebookSdk;
 import com.facebook.appevents.AppEventsLogger;
 
 import java.util.Arrays;
+import java.util.Collections;
 
 public class LoginActivity extends AppCompatActivity {
 
 
     private static final int RC_SIGN_IN = 123;
+    private SignInButton googleSignInButton;
+    private FirebaseAuth mAuth;
+    private String TAG = "LoginActivity";
 
 
     @Override
@@ -26,22 +31,23 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        this.startSignInWithGoogle();
+        googleSignInButton = findViewById(R.id.google_button);
+        mAuth = FirebaseAuth.getInstance();
 
+        googleSignInButton.setOnClickListener(v -> startSignInWithGoogle());
 
     }
 
-
-
+    //SIGN IN WITH GOOGLE
     private void startSignInWithGoogle(){
         startActivityForResult(
                 AuthUI.getInstance()
                         .createSignInIntentBuilder()
                         .setTheme(R.style.LoginTheme)
                         .setAvailableProviders(
-                                Arrays.asList(new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build()))
-                                    .setIsSmartLockEnabled(false, true)
-                                    .setLogo(R.drawable.common_google_signin_btn_icon_light).build(),
+                                Collections.singletonList(new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build())) //GOOGLE
+                        .setIsSmartLockEnabled(false, true)
+                        .build(),
                 RC_SIGN_IN);
     }
 
