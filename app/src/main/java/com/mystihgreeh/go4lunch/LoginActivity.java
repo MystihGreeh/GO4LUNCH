5630f19437338741d2cab4e943804c772c1483ef
MystihGreeh <charlotte.achin@gmail.com>
2020-12-14 16:56:51 +0100

    Initial commit

---
+++ app/src/main/java/com/mystihgreeh/go4lunch/LoginActivity.java
@@ -0,0 +1,50 @@
+package com.mystihgreeh.go4lunch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+
+import java.util.Arrays;
+
+public class LoginActivity extends AppCompatActivity {
+
+
+    private static final int RC_SIGN_IN = 123;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        this.startSignInWithGoogle();
+
+
+    }
+
+
+
+    private void startSignInWithGoogle(){
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setTheme(R.style.LoginTheme)
+                        .setAvailableProviders(
+                                Arrays.asList(new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build(),
+                                        new AuthUI.IdpConfig.Builder(AuthUI.FACEBOOK_PROVIDER).build()))
+                                    .setIsSmartLockEnabled(false, true)
+                                    .setLogo(R.drawable.common_google_signin_btn_icon_light).build(),
+                RC_SIGN_IN);
+    }
+
+
+}
