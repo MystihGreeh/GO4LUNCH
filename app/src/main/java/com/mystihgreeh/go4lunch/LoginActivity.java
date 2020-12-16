0c4deb8eb404ef4e19f43d3e9a7ddf00b97fd7b2
MystihGreeh <charlotte.achin@gmail.com>
2020-12-16 21:14:26 +0100

    Google and Facebook connexion done Bottom navigation view done Navigation drawer and Toolbar in progress

--- app/src/main/java/com/mystihgreeh/go4lunch/LoginActivity.java
+++ app/src/main/java/com/mystihgreeh/go4lunch/LoginActivity.java
@@ -1,41 +1,31 @@
 package com.mystihgreeh.go4lunch;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-
 import com.firebase.ui.auth.AuthUI;
 import com.google.android.gms.common.SignInButton;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.GoogleAuthProvider;
-import com.facebook.FacebookSdk;
-import com.facebook.appevents.AppEventsLogger;
-
-import java.util.Arrays;
 import java.util.Collections;
 
 public class LoginActivity extends AppCompatActivity {
 
 
     private static final int RC_SIGN_IN = 123;
-    private SignInButton googleSignInButton;
-    private FirebaseAuth mAuth;
-    private String TAG = "LoginActivity";
+    private final String TAG = "LoginActivity";
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        initIdentification();
 
-        googleSignInButton = findViewById(R.id.google_button);
-        mAuth = FirebaseAuth.getInstance();
 
+    }
+
+    private void initIdentification(){
+        SignInButton googleSignInButton = findViewById(R.id.google_button);
         googleSignInButton.setOnClickListener(v -> startSignInWithGoogle());
-
     }
 
     //SIGN IN WITH GOOGLE
