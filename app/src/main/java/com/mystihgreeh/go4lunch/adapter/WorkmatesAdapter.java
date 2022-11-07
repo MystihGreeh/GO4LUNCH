6d466dccf5e5b74e33c016a453eb437653b3aec2
MystihGreeh <charlotte.achin@gmail.com>
2022-11-07 14:25:10 +0100

    Click on the workmate in the workmateList is working (when a restaurant is picked)

--- app/src/main/java/com/mystihgreeh/go4lunch/adapter/WorkmatesAdapter.java
+++ app/src/main/java/com/mystihgreeh/go4lunch/adapter/WorkmatesAdapter.java
@@ -3,6 +3,7 @@ package com.mystihgreeh.go4lunch.adapter;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,6 +11,7 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
@@ -17,6 +19,7 @@ import com.bumptech.glide.request.RequestOptions;
 import com.google.firebase.database.annotations.NotNull;
 import com.mystihgreeh.go4lunch.R;
 import com.mystihgreeh.go4lunch.model.Workmates.Workmate;
+import com.mystihgreeh.go4lunch.ui.RestaurantDetailActivity;
 
 import java.util.ArrayList;
 import java.util.Objects;
@@ -66,6 +69,14 @@ public class WorkmatesAdapter extends RecyclerView.Adapter<WorkmatesAdapter.View
             holder.mWorkmateName.setTextColor(mContext.getResources().getColor(R.color.grey));
             holder.mWorkmateName.setText(workmate.getUsername() + " " + mContext.getString(R.string.has_not_decided));
         }
+        if (workmate.getRestaurantUid() != null){
+            holder.mRestaurantConstraintLayout.setOnClickListener(v -> {
+                Intent intent = new Intent(v.getContext(), RestaurantDetailActivity.class);
+                intent.putExtra("restaurantId", workmate.getRestaurantUid());
+                v.getContext().startActivity(intent);
+            });
+        }
+
     }
 
     @Override
@@ -78,6 +89,7 @@ public class WorkmatesAdapter extends RecyclerView.Adapter<WorkmatesAdapter.View
         public final ImageView mWorkmateAvatar;
         public final TextView mWorkmateName;
         public Workmate mItem;
+        public ConstraintLayout mRestaurantConstraintLayout;
 
 
         public ViewHolder(View view) {
@@ -85,6 +97,7 @@ public class WorkmatesAdapter extends RecyclerView.Adapter<WorkmatesAdapter.View
             mView = view;
             mWorkmateAvatar = view.findViewById(R.id.workmate_avatar);
             mWorkmateName = view.findViewById(R.id.workmate_name);
+            mRestaurantConstraintLayout = view.findViewById(R.id.list);
 
         }
 
